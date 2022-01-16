b88eeb67e4b3d69d9cb3232e4344f3ef4f4d6869
森裕輔 <yusukew3@gmail.com>
2022-01-17 07:06:41 +0900

    logo周り調整

--- src/NaviGation.tsx
+++ src/NaviGation.tsx
@@ -67,7 +67,7 @@ import {
             </Flex>
           </Flex>
   
-          <Stack
+          <Stack className='switch'
             flex={{ base: 1, md: 0 }}
             justify={'flex-end'}
             direction={'row'}
